5f1ac8f751e51bc29b69d155e1959fcdf07a6a0d
martin.carmona <martinweb15@gmail.com>
2023-09-26 08:39:44 +0200

    primero intento Comidas

--- javascript/index.js
+++ javascript/index.js
@@ -2,30 +2,18 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let resultadoProducto
-let resultadoPan
-let panes = ["Bollo", "Viena", "Pipas", "Mollete"]
-let productos = ["Mantequilla y mermelada", "Salchichón", "Tortilla", "Jamón y tomate", "York", "Carne mechada"]
+let resultadoComida
+let comidas = ["Macarrones con atún y cebolla", "Tortilla de patatas", 
+"Fiambre y huevos", "Puchero", "Fabada", "Cocido madrileño", "Filete y arroz", "Pizza",
+"Bocadillo de salchichas", "Pechuga empanada", "Salmorejo y postre", "Ensalada y postre"]
 
-let pan = document.getElementById("pan")
-let producto = document.getElementById("producto")
+let comida = document.getElementById("comidas")
 let resultado = document.getElementById("cajaResultado")
 
-pan.addEventListener("click", () => {
-    // console.log(panes[getRandomInt(panes.length)]);
-    resultadoPan = panes[getRandomInt(panes.length)]
-    resultado.innerHTML = "Hoy vas a desayunar " + resultadoPan
-
-})
-
-producto.addEventListener("click", () => {
-    // console.log(productos[getRandomInt(productos.length)]);
-    resultadoProducto = productos[getRandomInt(productos.length)]
-    if (!resultadoPan) {
-        resultado.innerHTML = "Seleciona primero el tipo de pan"
-    } else {
-        resultado.innerHTML = "Hoy vas a desayunar " + resultadoPan + " con " + resultadoProducto
-    }
+comida.addEventListener("click", () => {
+    // console.log(comidas[getRandomInt(comidas.length)]);
+    resultadoComida = comidas[getRandomInt(comidas.length)]
+    resultado.innerHTML = "Hoy vas a comer " + resultadoComida
 })
 
 
